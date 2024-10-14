a519c77b7ee91953281b8240c34a29bd1d407fd0
ZaidP101 <zpatel044@gmail.com>
2024-10-15 01:34:19 +0530

    JsonWebToken (JWT)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //register user
 const registerUser = asyncHandler(async (req,res) =>{
@@ -34,11 +35,29 @@ const registerUser = asyncHandler(async (req,res) =>{
 
 //login user
 const loginUser = asyncHandler(async (req,res) =>{
-    res.json({message: "User Login : "});
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory.");
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id   
+            },
+        },process.env.ACCESS_TOKEN_SECERT, {expiresIn:"1m"});
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401)
+        throw new Error("Email or Password invalid");
+    }
 });
 
 //current user
 const currentUser = asyncHandler(async (req,res) =>{
-    res.json({message: "Current User : "});
+    res.json(req.user);
 });
 module.exports = {registerUser,loginUser,currentUser}
